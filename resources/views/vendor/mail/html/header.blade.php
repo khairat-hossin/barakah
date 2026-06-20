482b65e30942f4b3b4d252c8044fcfde767886dc
Khairat Hossin <khairat564@gmail.com>
2026-06-21 00:54:27 +0600

    Email: Brand the mail header with logo-name image
    Published Laravel mail markdown components and replaced the header text with
    the organization logo (logo-name.png) so all emails (deposit/expense receipts,
    reminders) show a branded header like a standard invoice email.

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('assets/logo/logo-name.png') }}" alt="{{ config('app.name') }}" style="height: 48px; max-height: 48px; width: auto;">
+</a>
+</td>
+</tr>
